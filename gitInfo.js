1960d6a3296f961d03c14f23fb7a95d605e329bb
Tori Daughton <toriddaughton@gmail.com>
2022-10-22 21:28:01 -0600

    initial commit

---
+++ gitInfo.js
@@ -0,0 +1,79 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+const gitDefinition = 'Git is a version control system (program, local). Allows individuals and teams to track changes to code projects Prevents bugs and loss of progress. Allows you to view the long history of a project in snapshots'
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+const gitHubDefinition = 'GitHub is a website. It allows you to take your git repositories and upload/download them. It is essentially a cloud service for your git repositories.'
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create a variable called 'gitInitDefinition'.  
+    It should be a string containing your best definition of what 'git init' does.
+*/
+
+const gitInitDefinition = 'Git init initializes a local git repository right here in this particular folder'
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create a variable called 'gitCloneDefinition'.  
+    It should be a string containing your best definition of what 'git clone' does.
+*/
+
+const gitCloneDefinition = 'Git Clone takes a repository and makes a clone of it'
+
+//////////////////PROBLEM 5////////////////////
+/*
+    Create a variable called 'gitStatusDefinition'.  
+    It should be a string containing your best definition of what 'git status' does.
+*/
+
+const gitStatusDefinition = 'Receives a status of the specific git repository: "tell me about this repository"'
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create a variable called 'gitAddDefinition'.  
+    It should be a string containing your best definition of what 'git add' does.
+
+    Create another variable called 'gitAddCode'.  
+    It should be a string containing the code to add all files.
+*/
+
+const gitAddDefinition = 'Adds particular files to be tracked / staged'
+
+const gitAddCode = 'git add .'
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create a variable called 'gitCommitDefinition'.  
+    It should be a string containing your best definition of what 'git commit' does.
+
+    Create a variable called 'gitCommitCode'.  
+    It should be a string containing the code to commit using the message "initial commit".
+*/
+
+const gitCommitDefinition = 'Creates a snapshot of the files being tracked'
+
+const gitCommitCode = 'git commit -m "initial commit"'
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create a variable called 'gitPushDefinition'.  
+    It should be a string containing your best definition of what 'git push' does.
+*/
+
+const gitPushDefinition = 'After adding the files and creating the screenshot pushing allows the local repository to be sent to the remote repository (GitHub website)'
---
+++ sdlc.js
@@ -0,0 +1,31 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+    The first one is done for you. 
+*/
+const planning = "Planning is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+
+const analysisOfRequirements = "Analysis of Requirements is the process of defining user expectations for a new software being built or modified."
+
+const design = "Design is the process of envisioning and defining software solutions to one or more sets of problems"
+
+const implementation = "Implementation is the process of adopting and integrating a software application into a business workflow"
+
+const testingAndIntegration = "Testing the functionality of the code and integrating changes. It is the process of testing the interface between two software units or modules. "
+
+const management = "Follow up care. Management is the application of management activities - planning, coordinating, measuring, monitoring, controlling, and reporting - to ensure that the development and maintenance of software is systematic, disciplined, and quantified."
